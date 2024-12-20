130f0341b9b7b0a0d29556fbb66d8f4feeb04ab3
user <alinkaa0301@gmail.com>
2024-12-20 22:03:40 +0200

    output of results

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ class Calculator:
         if second == 0:
             return "Error"
         return first / second
+
+calc = Calculator()
+first = 25
+second = 5
+print("calculator:")
+print(calc.add(first, second))
+print(calc.subtract(first, second))
+print(calc.multiply(first, second))
+print(calc.divide(first, second))
